75252a2b532b81691067cec1494d335428cd3288
Rockair <kiraly.tamas@outlook.com>
2016-08-27 22:43:17 +0200

    ok

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
 
 var websocket = require('ws');
 var ws = new websocket("ws://fc1.intelilight.eu:6080/JSON",{protocol: "protocolOne"});
+
+console.log("end");
+
+
+ws.on('open', function open() {
+  console.log("open");
+});
+ 
+ws.on('message', function(data, flags) {
+  console.log(data); 
+});
