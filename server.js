b3d7aacdf60b571646088b335ef3372a6965feb5
Rockair <kiraly.tamas@outlook.com>
2016-08-27 22:23:49 +0200

    first commit

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 
 var websocket = require('ws');
-var ws = new websocket("ws://fc1.intelilight.eu:6080/JSON");
-
+var ws = new websocket("ws://fc1.intelilight.eu:6080/JSON", {
+  protocol: "protocolOne"
+});
