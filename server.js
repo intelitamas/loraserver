c61f1892014eb14b2a9e575cf302656851634a33
Rockair <kiraly.tamas@outlook.com>
2016-08-27 22:48:11 +0200

    ok

--- server.js
+++ server.js
@@ -12,3 +12,50 @@ ws.on('open', function open() {
 ws.on('message', function(data, flags) {
   console.log(data); 
 });
+
+var http = require('http');
+ var fs = require('fs');
+ var path = require('path');
+
+ http.createServer(function (request, response) {
+
+    console.log('request starting for ');
+    console.log(request);
+
+    var filePath = '.' + request.url;
+    if (filePath == './')
+        filePath = './index.html';
+
+    console.log(filePath);
+    var extname = path.extname(filePath);
+    var contentType = 'text/html';
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+    }
+
+    path.exists(filePath, function(exists) {
+
+        if (exists) {
+            fs.readFile(filePath, function(error, content) {
+                if (error) {
+                    response.writeHead(500);
+                    response.end();
+                }
+                else {
+                    response.writeHead(200, { 'Content-Type': contentType });
+                    response.end(content, 'utf-8');
+                }
+            });
+        }
+        else {
+            response.writeHead(404);
+            response.end();
+        }
+    });
+
+ }).listen(process.env.PORT);
